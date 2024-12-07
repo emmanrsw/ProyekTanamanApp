f60def7ab3a0bcb9b40086e903496a7b7dce8dc7
adilaputri <margaretanovianti79@gmail.com>
2024-12-07 11:36:54 +0700

    benerin search kalau di reload langsung clear dan benerin rata kiri tombol register dan login di page register

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -63,7 +63,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#"><span>Tanam</span><span class="highlight">.in</span></a>
+    <a class="navbar-brand" href="{{ Auth::guard('pelanggan')->check() ? route('home') : route('register') }}"><span>Tanam</span><span class="highlight">.in</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
